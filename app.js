c89f1f629fc84d7279311290ce071b3757dfc64d
ManishPoduval <manishpoduval@hotmail.com>
2024-01-09 14:55:41 +0530

    lesson code

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require("./config")(app);
 const projectName = "project-management-server";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
+app.locals.title = `${capitalized(projectName)} created with Rootlauncher`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
